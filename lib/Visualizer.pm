0cd4f3fde68cca74a3421ae29570c2b200fa633c
punytan <punytan@gmail.com>
2014-04-17 11:37:03 +0900

    init

---
+++ lib/Visualizer.pm
@@ -0,0 +1,39 @@
+package Visualizer;
+
+use strict;
+use 5.008_005;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Visualizer - Blah blah blah
+
+=head1 SYNOPSIS
+
+  use Visualizer;
+
+=head1 DESCRIPTION
+
+Visualizer is
+
+=head1 AUTHOR
+
+punytan E<lt>punytan@gmail.comE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2014- punytan
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
